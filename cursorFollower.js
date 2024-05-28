1b5f690191bb755b3365fd388942808e938de279
Chetan Singh Rajawat <=chetan.rajawat25@gmail.com>
2024-05-28 20:02:41 +0530

    fixing bugs in mouse followers

--- cursorFollower.js
+++ cursorFollower.js
@@ -1,10 +1,12 @@
-let playReel = document.querySelectorAll("#playreel");
+let playReel =  document.querySelector("#playreel");
+let takeOne = document.querySelector("#playreel.take-one");
 
-let page1 = playReel[0].parentNode;
-let page2 = playReel[1].parentNode;
+let page1 = playReel.parentNode;
 
+let page2 = takeOne.parentNode;
+console.log(page2);
 
-console.log(page2)
+// console.log(page2)
 // let handleMouseMove = (dets) =>{
 //     playReel.style.left = dets.x +  "px";
 //     playReel.style.top = dets.y +  "px";
@@ -14,11 +16,13 @@ console.log(page2)
 let handleMousemove = (dets) => { 
     // console.log(dets);
     gsap.to(playReel,{
-        x:dets.x,
-        y:dets.y,
+        x:dets.pageX,
+        y:dets.pageY,
         scale:1
     })
 }
+
+
 
 
 let handleMouseEnter = ()=>{
@@ -35,6 +39,32 @@ let handleMouseLeave = ()=>{
 }
 
 
+let handleMousemove2 = (dets) => { 
+    // console.log(dets);
+    gsap.to(takeOne,{
+        x:dets.pageX,
+        y:dets.pageY,
+        scale:1
+    })
+}
+
+
+
+
+let handleMouseEnter2 = ()=>{
+    gsap.to(takeOne,{
+        scale:1
+    })
+   
+}
+
+let handleMouseLeave2 = ()=>{
+    gsap.to(takeOne,{
+        scale:0
+    })
+}
+
+
 page1.addEventListener("mousemove" , handleMousemove);
 
 
@@ -43,9 +73,9 @@ page1.addEventListener("mouseenter" , handleMouseEnter);
 page1.addEventListener("mouseleave" , handleMouseLeave);
 
 
-page2.addEventListener("mousemove" , handleMousemove);
+page2.addEventListener("mousemove" , handleMousemove2);
 
 
 
-page2.addEventListener("mouseenter" , handleMouseEnter);
-page2.addEventListener("mouseleave" , handleMouseLeave);
+page2.addEventListener("mouseenter" , handleMouseEnter2);
+page2.addEventListener("mouseleave" , handleMouseLeave2);
